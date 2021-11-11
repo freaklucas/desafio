c5b72e071d3f8481fdd76905aa81ec755ecb28da
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-11 19:35:56 -0300

    header ok

---
+++ src/components/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/template/Header.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+  },
+};
+</script>
