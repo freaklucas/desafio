2b3713c837418a82bba9bf4efa83c4882b4d3e4d
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-10 23:43:07 -0300

    init config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  screens: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+    "2xl": "1536px",
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
