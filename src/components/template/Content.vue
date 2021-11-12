f0b5676462e13802c11890538e0bf9090523b628
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-12 00:23:23 -0300

    separando display content

--- src/components/template/Content.vue
+++ src/components/template/Content.vue
@@ -15,6 +15,52 @@
       >
         {{ des.text }}
       </div>
+      <div class="flex align-center mt-12">
+        <div class="lg:w-2/3 md:w-2/3 mr-8 ml-4 sm:px-6 lg:px-4 center">
+          <div class="flex">
+            <div class="flex-nowrap ml-16">
+              <form
+                @submit.prevent="onSubmit"
+                class="w-full center max-w-lg mr-8"
+              >
+                <div class="flex flex-row -mx-5 mb-6"></div>
+              </form>
+            </div>
+          </div>
+          <div class="card-table">
+            <div class="flex mb-12">
+              <table
+                class="
+                  roundend-t-sm
+                  text-sm
+                  w-5/6
+                  mx-auto
+                  font-mono
+                  text-gray-800
+                "
+              >
+                <tr class="text-left border-b-2 border-gray-300">
+                  <th class="px-4 py-3">Código</th>
+                  <th class="px-4 py-3">Nome</th>
+                  <th class="px-4 py-3">Caminho</th>
+                </tr>
+                <tr
+                  class="bg-gray-100 border-b border-gray-200"
+                  v-for="list in lists"
+                  :key="list"
+                >
+                  <td class="px-4 py-3">{{ (counter += 1) }}</td>
+                  <td class="px-4 py-3">{{ list.name }}</td>
+                  <td class="px-4 py-3">{{ list.path }}</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-2/3 w-2/3 mt-4 lg:mt-2 pl-4 ml-4">
+          <a>Conteudo do gráfico...</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
